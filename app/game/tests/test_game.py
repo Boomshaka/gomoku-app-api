321cb6fcb41696a7678172de4d051d55f20db47c
Shaka Kanenobu <shakakanenobu@gmail.com>
2021-01-02 17:21:49 -0800

    Added test case for GET game

--- app/game/tests/test_game.py
+++ app/game/tests/test_game.py
@@ -6,6 +6,13 @@ class GameTests(TestCase):
     def setUp(self):
         self.client = Client()
 
+    def test_fetch_game_successful(self):
+        """Test that game with id exists"""
+        new_game = self.client.post('/game/')
+        game_id = new_game.data['id']
+        game = self.client.get(f'/game/{game_id}')
+        self.assertContains(game,'id',count=1,status_code=200)
+
     def test_create_game_successful(self):
         """Test creating a new game is successful"""
         game = self.client.post('/game/')
