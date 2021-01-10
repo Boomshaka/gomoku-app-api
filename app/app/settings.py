13eb921507266ad1467476cce2fa9be513ab4285
Shaka Kanenobu <shakakanenobu@gmail.com>
2021-01-10 00:26:36 -0800

    Add heroku.yml

--- app/app/settings.py
+++ app/app/settings.py
@@ -124,5 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 #disabled static
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
