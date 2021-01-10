fac98c1a8f1178688a8985336ea356d28aaebb2e
Shaka Kanenobu <shakakanenobu@gmail.com>
2021-01-10 01:39:01 -0800

    reconfig db

--- app/app/settings.py
+++ app/app/settings.py
@@ -127,9 +127,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# import dj_database_url
+import dj_database_url
 # # DATABSE_URL = 'postgresql://<postgresql>'
-# # DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 # DATABASES = {
 #     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600, ssl_require=True)
 # }
