19902dedbd35ca7c3e25d544d51647aec5ec6ad9
Shaka Kanenobu <shakakanenobu@gmail.com>
2021-01-10 02:06:32 -0800

    reconfig db

--- app/app/settings.py
+++ app/app/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 import dj_database_url
 # # DATABSE_URL = 'postgresql://<postgresql>'
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-# DATABASES = {
-#     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600, ssl_require=True)
-# }
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 # DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql'
