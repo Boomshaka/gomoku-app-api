f29f4a58f5b12ae0afa0e9c80ee116bcdc1c56d4
Shaka Kanenobu <shakakanenobu@gmail.com>
2021-02-21 03:05:16 -0800

    wait for db

--- app/app/settings/ci.py
+++ app/app/settings/ci.py
@@ -15,5 +15,6 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
+        'PORT': '5432',
     }
 }
