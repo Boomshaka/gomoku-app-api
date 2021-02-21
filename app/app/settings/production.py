40e7bd1a6328399188e8c1e98faddea7b126f24b
Shaka Kanenobu <shakakanenobu@gmail.com>
2021-02-21 02:33:31 -0800

    Revert "updated api endpoint" revert This reverts commit f6e2cac56b9b7da378a0ae9b605abdb42e5d26fb.

--- app/app/settings/production.py
+++ app/app/settings/production.py
@@ -5,7 +5,6 @@ import dj_database_url
 
 DEBUG = True
 ALLOWED_HOSTS = ['gomoku-api.herokuapp.com']
-# ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
